523e6d59eecd3bb294d8a366da6ed0b39759b050
mitvoss <mitchellvos93@gmail.com>
2021-06-16 15:40:17 +0200

    done

--- conditionals.js
+++ conditionals.js
@@ -8,25 +8,29 @@ console.log('Is 2 gelijk aan 2?', equalNumbers);
 // ==========================================
 // 1. Declareer een variabele en sla daar de uitkomst van de vergelijking "Is 10 groter dan 20?" in op        // geeft false wanneer gelogd
 // ==========================================
-
-
+const biggerThen = 10 > 20;
+console.log('is 10 groter dan 20?', biggerThen)
 // ==========================================
 // 2. Declareer een variabele en sla daar de uitkomst van de vergelijking "Is 10 ongelijk aan 11?" in op        // geeft true wanneer gelogd
 // ==========================================
-
+const equalTo = 10 != 11;
+console.log('is 10 ongelijk aan 11?', equalTo)
 
 // ==========================================
 // 3. Declareer een variabele en sla daar in op of de woorden 'zoet' en 'sappig' hetzelfde zijn                // geeft false wanneer gelogd
 // ==========================================
-
+const zs = "zoet" === "sappig"
+console.log("Zijn zoet en sappig hetzelfde?", zs)
 
 // ==========================================
 // 4a. Declareer een variabele en sla daar het rapportcijfer 9 in op.
 // 4b. schrijf een script dat checkt of het rapportcijfer groter is dan 8.
 //     Als dat zo is, loggen we 'Cum laude!' in de console.                                                     // geeft 'Cum laude!' wanneer gelogd
 // ==========================================
-
-
+const rGrade = 9;
+    if (rGrade > 8){
+        console.log("Cumlaude!")
+    }
 // ==========================================
 // 5a. Gebruik onderstaande variabelen isRaining en temperature;
 // 5b. Schrijf een script dat "Je kunt een korte broek aan!" logt wanneer het niet regent EN de temperatuur hoger is dan 21 graden
@@ -34,5 +38,14 @@ console.log('Is 2 gelijk aan 2?', equalNumbers);
 // Tip: verander de waarde van isRaining en temperature even handmatig om te kijken of jouw 2e conditie klopt
 // ==========================================
 
-const isRaining = false;
-const temperature = 23;
+let isRaining = false;
+let temperature = 24;
+
+ if (!isRaining && temperature >= 21) {
+     console.log("Je kunt een korte broek aan!")
+ }
+
+ if (isRaining || temperature <12){
+     console.log("Trek een jas aan!")
+}
+1
--- switch-statements.js
+++ switch-statements.js
@@ -4,6 +4,36 @@
 
 const errorCode = 0;
 
+
+    switch (errorCode) {
+            case 0:
+                console.log("Geen watertoevoer")
+                    break;
+
+            case 1:
+                console.log("Tempratuur te laag")
+                    break;
+
+            case 2:
+                console.log( "Koffiebonen Op")
+                    break;
+
+            case 3:
+                console.log( "Afvalbak Vol")
+                    break;
+
+            case 4:
+                console.log("Geen druk")
+    }
+
+    /*const errorCode = 1;
+    console.log( "Tempratuur te laag")
+const errorCode = 2
+    console.log( "Koffiebonen Op")
+const errorCode = 3
+    console.log( "Afvalbak Vol")
+const errorCode = 4
+    console.log("Geen druk")*/
 // Je mag ervanuit gaan dat deze variabele altijd een waarde tussen de 0 en de 4 bevat. Deze codes hebben de volgende betekenissen (foutmeldingen):
 // 0: "Geen watertoevoer"
 // 1: "Temperatuur te laag"
@@ -21,7 +51,24 @@ const errorCode = 0;
 // Opdracht 2: schrijf een switch statement die op basis van de volgende variabele een bijbehorende foutmelding in de console logt.
 // ==========================================
 
-const monitorColor = "red";
+const monitorColor = "green";
+
+    switch (monitorColor) {
+        case "red":
+            console.log("Kritieke hartslag, actie vereist!")
+            break
+        case "yellow":
+            console.log("Hartslag verhoogd")
+            break
+        case "orange":
+            console.log("Kritieke hartslag, actie vereist!")
+            break
+        case "green":
+            console.log("Hartslag stabiel")
+            break
+        default:
+            console.log("Status onbekend")
+    }
 
 // Deze variabele zal altijd een kleur bevatten. De specifieke kleuren waar een melding bij gemaakt moet worden zijn: "red", "green", "yellow" en "orange".
 // Deze codes zijn gelinkt aan de volgende meldingen:
